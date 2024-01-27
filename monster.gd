405ab574efa08872d6540888ecdf696b46c29190
Chu Huy Quang <quangdtm2004@gmail.com>
2024-01-27 15:30:29 +0700

    update

--- monster.gd
+++ monster.gd
@@ -1,5 +1,5 @@
 class_name Monster
-extends Node2D
+extends CharacterBody2D
 
 @export var tickle: Array[TicklePoint]
 
--- player.gd
+++ player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 signal dodge
 var in_dodge_time: bool = false
@@ -6,27 +6,21 @@ var enemy
 var pos: Vector2
 const moveset_attack = [[1,0,0], [0,0,1]]
 const moveset_block = [[0,1,1], [1,1,0]]
+const SPEED = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pos = $Icon.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	var direction = Input.get_axis("dodge_left", "dodge_right")
-	if direction and not in_dodge_time:
-		dodge.emit()
-		$Icon.position = Vector2(584 + direction * 184, 512)
-		if direction < 0:
-			direction = 0
-		$Block.set_block(moveset_block[direction])
-		in_dodge_time = true
-		await get_tree().create_timer(1.0).timeout
-		in_dodge_time = false
-		$Block.set_default()
-		$Icon.position = Vector2(584, 512)
-
-
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	look_at(get_global_mouse_position())
+	move_and_slide()
 
 func is_in_attack_time() -> bool:
 	return $Attack.in_attack_time
